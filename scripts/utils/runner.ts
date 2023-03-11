ed4445871ff2edc14419f5df67835c67ff3b33b4
pilagod <pilagooood@gmail.com>
2023-03-11 17:54:44 +0800

    remove scripts

--- scripts/utils/runner.ts
+++
@@ -1,8 +0,0 @@
-export function run(action: (...args: any) => Promise<any>) {
-    action()
-        .then(() => process.exit(0))
-        .catch((e) => {
-            console.error(e)
-            process.exit(1)
-        })
-}
