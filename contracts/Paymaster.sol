7c8c9631c0ecafbc6e73502329a97cbe05fe69bc
pilagod <pilagooood@gmail.com>
2023-03-24 16:37:25 +0800

    add more explorations

--- contracts/Paymaster.sol
+++ contracts/Paymaster.sol
@@ -17,6 +17,7 @@ contract Paymaster is BasePaymaster {
 
     uint256 count = 0;
 
+    uint256 lastBalance = 0;
     uint256 lastBlockNumber = 0;
     uint256 lastBlockTimestamp = 0;
     uint256 lastGasPrice = 0;
@@ -29,6 +30,8 @@ contract Paymaster is BasePaymaster {
         bytes32 userOpHash,
         uint256 maxCost
     ) internal override returns (bytes memory context, uint256 validationData) {
+        (userOp, userOpHash, maxCost);
+
         address paymaster = address(bytes20(userOp.paymasterAndData[:20]));
         bytes memory data = userOp.paymasterAndData[20:];
         emit Log(paymaster, data);
@@ -40,7 +43,7 @@ contract Paymaster is BasePaymaster {
         // count += 1;
 
         // [Pass] no matter stake or not
-        IERC20(token).balanceOf(userOp.sender);
+        uint256 balance = IERC20(token).balanceOf(userOp.sender);
 
         // [Error] unstaked paymaster accessed 0x7aedf613f6865fb7d74147768757b66c1bd7fbdc slot 0x8a1c82de58ad310be7c440d8bd60d39c7347bb7953c619d869bdf592c973e5bf
         // [Pass] after paymaster stake > MIN_STAKE_VALUE (configured by bundler)
@@ -49,6 +52,9 @@ contract Paymaster is BasePaymaster {
 
         // [Error] paymaster has forbidden read from 0x7fd7e7cb7b54abced3183cc7a78ea7be78b49eec slot 0x7ce134a961d78589dec823a6f536bb188e0713eab16baf472e1020b44cb6554a
         // IERC20(token).balanceOf(address(entryPoint));
+
+        // [Error] paymaster uses banned op code: SELFBALANCE
+        // lastBalance = address(this).balance;
 
         // [Error] paymaster uses banned op code: NUMBER
         // lastBlockNumber = block.number;
@@ -62,22 +68,25 @@ contract Paymaster is BasePaymaster {
         // [Error] paymaster uses banned op code: GAS
         // lastGasLeft = gasleft();
 
-        // [Pass] but these can pass
-        uint256 blockNumber = block.number;
-        uint256 blockTimestamp = block.timestamp;
-        uint256 gasPrice = tx.gasprice;
-        uint256 gasLeft = gasleft();
-        uint256 balance = address(this).balance;
+        // [Error]
+        //   * when update return values (context, validationData) afterward
+        //   * when update storage afterward
+        //   * when if block contains revert/return statement
+        // [Pass] otherwise the op codes will pass
+        // uint256 blockNumber = block.number;
+        // if (block.number > balance) {
+        //     revert("reason");
+        //     return (bytes(""), 0);
+        // }
 
-        (userOp, userOpHash, maxCost);
-        return (bytes(""), 0);
+        context = abi.encode(userOp.sender, token);
     }
 
     function _postOp(
         PostOpMode mode,
         bytes calldata context,
         uint256 actualGasCost
-    ) internal pure override {
+    ) internal override {
         (mode, context, actualGasCost); // unused params
     }
 }
